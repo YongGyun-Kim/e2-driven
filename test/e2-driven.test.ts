a44fa333e6867d2ce3cc1b6cb8f49a99dbe7be8d
YongGyun-Kim <kyg7089@naver.com>
2025-01-02 23:12:07 +0900

    init commit

---
+++ test/e2-driven.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as E2Driven from '../lib/e2-driven-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/e2-driven-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new E2Driven.E2DrivenStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
